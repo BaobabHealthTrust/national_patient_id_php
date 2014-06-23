630b03c5fb115b71825b30697ea6573f816ee5ff
Blessings <blessings.mhango@gmail.com>
2014-06-23 12:32:16 +0200

    calling methods

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
  
  if (isset($_POST['check_national_id'])) {
+   require_once('includes/national_patient_id.php');
+   $national_id = $_POST['national_id'];
+   $national_id_decimal = to_decimal($_POST['national_id']);
+  
+   if (is_valid($national_id_decimal)){
+     
+    echo $national_id . " is a valid national id";
+
+   }else{
+   
+     echo $national_id . " is not a valid national id";
+    
+    }
   
   }
-
 ?>
 
 <html>
@@ -15,8 +27,5 @@
 <p><label for="national_id">Enter National ID: </label><input type="text" id="national_id" name="national_id"></p>
 <p><input type="submit" id="check_national_id" name="check_national_id" value="Validate National ID" /></p>
 </form>
-<pre>
-<?php if (isset($_POST['check_national_id'])) {print_r($_POST);} ?>
-</pre>
 </body>
 </html>
