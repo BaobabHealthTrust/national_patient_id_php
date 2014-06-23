94b401a24ecabe138bbe1ef002d138f74046c8dc
Blessings <blessings.mhango@gmail.com>
2014-06-23 11:41:36 +0200

    working on to decimal method

--- includes/national_patient_id.php
+++ includes/national_patient_id.php
@@ -1,87 +1,48 @@
-<? php
-class NationalPatientId {
+<?php
 
-  private static $base_map = ['0','1','2','3','4','5','6','7','8','9','A','C','D','E','F','G',
-                'H','J','K','L','M','N','P','R','T','U','V','W','X','Y'];
-              
-  private static $reverse_map = {'0' => 0,'1' => 1,'2' => 2,'3' => 3,'4' => 4,'5' => 5,
-                   '6' => 6,'7' => 7,'8' => 8,'9' => 9,
-                   'A' => 10,'C' => 11,'D' => 12,'E' => 13,'F' => 14,'G' => 15,
-                   'H' => 16,'J' => 17,'K' => 18,'L' => 19,'M' => 20,'N' => 21,
-                   'P' => 22,'R' => 23,'T' => 24,'U' => 25,'V' => 26,'W' => 27,
-                   'X' => 28,'Y' => 29};
+  function check_digit($number){
+    settype($number, 'string');
+		$sumTable = array(array(0,1,2,3,4,5,6,7,8,9),array(0,2,4,6,8,1,3,5,7,9));
+		$sum = 0;
+		$flip = 0;
+		for ($i = strlen($number) - 1; $i >= 0; $i--) {
+				$sum += $sumTable[$flip++ & 0x1][$number[$i]];
+		}
 
-  private $base;
-  private $value;
-
-  //Create a National Patient ID
-  //
-  // <tt>num</tt> is the decimal equivalent of the Base <tt>base</tt> Id to be
-  // created without the check digit
-  function init($num, $src_base = 10, $base = 30){
-    if (num && src_base == 10){
-      $num = $num.to_i;
-      $num = $num * 10 + self::check_digit($num);
-    end
-    $decimal_id = self::to_decimal($num, $src_base);
-    this->$base = $base;
-    this->$value = self::convert(@decimal_id).rjust(6,'0');
-  }
-
+		return $sum % 10 === 0;
   
-  public static function check_digit($number) {
-    $number = $number.to_s;
-    $number = $number.split(//).collect { |digit| digit.to_i };
-    $parity = $number.length % 2;
-
-    $sum = 0;
-    //$number.each_with_index do |digit,index|
-    //  digit = digit * 2 if index%2==parity
-    //  digit = digit - 9 if digit > 9
-    //  sum = sum + digit
-    //end
-
-    $checkdigit = 0;
-    $checkdigit = $checkdigit +1 while (($sum+($checkdigit))%10)!=0
-    return $checkdigit;
   }
 
-  // Convert a Base 10 <tt>number</tt> to the specified <tt>base</tt>
-  function convert($num){
-    $results = '';
-    $quotient = $num.to_i;
-      
-    while quotient {
-      $results = $base_map[$quotient % $base] + $results;
-      $quotient = ($quotient / $base);
-    }
-    return $results;
+  function to_decimal($num, $from_base=30){		        
+			 $reverse_map = array('0' => 0,'1' => 1,'2' => 2,'3' => 3,'4' => 4,'5' => 5,
+						             '6' => 6,'7' => 7,'8' => 8,'9' => 9,
+						             'A' => 10,'C' => 11,'D' => 12,'E' => 13,'F' => 14,'G' => 15,
+						             'H' => 16,'J' => 17,'K' => 18,'L' => 19,'M' => 20,'N' => 21,
+						             'P' => 22,'R' => 23,'T' => 24,'U' => 25,'V' => 26,'W' => 27,
+						             'X' => 28,'Y' => 29);
+
+			 $number = array_reverse(str_split(str_replace('-','',$num)));
+			 $number2;
+				
+			 foreach ($number as $key => $value){
+			 		$number2 +=  $reverse_map[$value] * (pow($from_base,$key));
+				}
+
+			 return $number2;
   }
 
-   //When converting to string, print a hyphen after the third character
-  function to_s() {
-    //"#{@value.slice(0,3)}#{@@separator}#{@value.slice(3,@value.length)}"
-  }
-
- 
-  // Convert given <tt>num</tt> in from the specified <tt>from_base</tt> to
-  // decimal (base 10)
-  def self.to_decimal(num, from_base=30){
-    $decimal = 0
-    //num.to_s.gsub(@@separator, '').split('').reverse.each_with_index do |n, i|
-    //  decimal += @@reverse_map[n] * (from_base ** i)
-    //end
-    return $decimal;
-  }
-
-  //Checks if <tt>num<tt> has a correct check digit
-  public static function valid($num) {
+  # Checks if <tt>num<tt> has a correct check digit
+  function is_valid($num){
     $core_id = $num / 10;
     $check_digit = $num % 10; # last digit
 
-    $check_digit == self::check_digit($core_id)
-    return $check_digit;
+    if ($check_digit == check_digit($core_id)){
+       return 1;  
+    }
+    else
+    { 
+      return 0;
+    }
   }
 
-}
 ?>
