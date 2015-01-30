e5017e0afed2258d02766ddd413457b41187ec5f
blessings <blessings.mhango@gmail.com>
2015-01-30 14:44:49 +0200

    validating id length

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 
-<form method="post" action="">
+<form id="myform" method="post" action="">
 <h2>National Patient ID Validator</h2>
 <?php
  error_reporting(E_ALL ^ E_NOTICE);
@@ -12,27 +12,37 @@
  if (isset($_POST['check_national_id'])) {
   if ($_POST['national_id']) {
    require_once('includes/national_patient_id.php');
-   $national_id = $_POST['national_id'];
-   $national_id_decimal = to_decimal($_POST['national_id']);
+   $national_id = str_replace('-','',$_POST['national_id']);
+   $national_id_decimal = to_decimal($national_id);
   
    if (is_valid($national_id_decimal)){
      
-    echo "<p p style='color:green'>" . $national_id . " is a valid national id.</p>" ;
+    echo "<p style='color:green'>" . $national_id . " is a valid national id.</p>" ;
 
    }else{
    
-     echo "<p p style='color:orange'>" . $national_id . " is not a valid national id.</p>";
+     echo "<p style='color:orange'>" . $national_id . " is not a valid national id.</p>";
     
     }
-   }else{
-     
-     echo "<p style='color:red'>Please enter national id.</p>" ;
- 
-    }
+   }
+  
   }
 ?>
 <p><label for="national_id">Enter National ID: </label><input type="text" id="national_id" name="national_id"></p>
-<p><input type="submit" id="check_national_id" name="check_national_id" value="Validate National ID" /></p>
+<p><input type="submit" id="check_national_id" name="check_national_id" onclick="validateLength()" value="Validate National ID" /></p>
+
 </form>
+<script type="text/javascript">
+function validateLength() {
+  
+   var national_id = document.getElementById("national_id");
+   var national_id_stripped = national_id.value.replace('-','');
+   if (national_id_stripped.length != 6){
+    alert("The National ID can only be six characters long");
+   }
+  
+}
+
+</script>
 </body>
 </html>
